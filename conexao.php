76289d9a0e9038752a8ba6c729f6837386a7c234
Gab7Brbosa <gabriel.silva1521@aluno.ce.gov.br>
2026-05-14 11:54:52 -0300

    ajeitando a tabela

--- conexao.php
+++ conexao.php
@@ -1,3 +1,3 @@
 <?php
-$conect = new PDO("mysql:host=localhost;dbname=projeto_PW", "root", "");
+$conect = new PDO("mysql:host=localhost;dbname=projeto_pw", "root", "");
 $conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
